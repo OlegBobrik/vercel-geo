ebf8035c34b8a9f4308f011fa60085cbf61cefb6
Oleg Bobrik <oleg.bobrik@byteminds.co.uk>
2023-03-09 10:02:27 +0500

    Redirect

--- middleware.ts
+++ middleware.ts
@@ -1,30 +1,44 @@
-import { NextRequest, NextResponse } from 'next/server'
-import countries from './lib/countries.json'
+import { geolocation } from '@vercel/edge';
+import { NextRequest, NextResponse } from 'next/server';
+// import countries from './lib/countries.json';
 
 // run only on homepage
 export const config = {
   matcher: '/',
 }
 
-export async function middleware(req: NextRequest) {
-  const { nextUrl: url, geo } = req
-  const country = geo.country || 'US'
-  const city = geo.city || 'San Francisco'
-  const region = geo.region || 'CA'
+export async function middleware(request) {
+  const url = new URL(request.url);
 
-  const countryInfo = countries.find((x) => x.cca2 === country)
+  const { country } = geolocation(request);
+  // You can also get the country using dot notation on the function
+  // const country = geolocation(request).country;
 
-  const currencyCode = Object.keys(countryInfo.currencies)[0]
-  const currency = countryInfo.currencies[currencyCode]
-  const languages = Object.values(countryInfo.languages).join(', ')
+  if (country === 'RU') {
+    url.pathname = 'https://google.com';
+  } else {
+    url.pathname = 'https://www.youtube.com';
+  }
 
-  url.searchParams.set('country', country)
-  url.searchParams.set('city', city)
-  url.searchParams.set('region', region)
-  url.searchParams.set('currencyCode', currencyCode)
-  url.searchParams.set('currencySymbol', currency.symbol)
-  url.searchParams.set('name', currency.name)
-  url.searchParams.set('languages', languages)
 
-  return NextResponse.rewrite(url)
+  // const { nextUrl: url, geo } = req
+  // const country = geo.country || 'US'
+  // const city = geo.city || 'San Francisco'
+  // const region = geo.region || 'CA'
+  //
+  // const countryInfo = countries.find((x) => x.cca2 === country)
+  //
+  // const currencyCode = Object.keys(countryInfo.currencies)[0]
+  // const currency = countryInfo.currencies[currencyCode]
+  // const languages = Object.values(countryInfo.languages).join(', ')
+  //
+  // url.searchParams.set('country', country)
+  // url.searchParams.set('city', city)
+  // url.searchParams.set('region', region)
+  // url.searchParams.set('currencyCode', currencyCode)
+  // url.searchParams.set('currencySymbol', currency.symbol)
+  // url.searchParams.set('name', currency.name)
+  // url.searchParams.set('languages', languages)
+
+  return NextResponse.redirect(url)
 }
